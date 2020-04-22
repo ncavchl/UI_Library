a92d49235072baf671e3b95fe0df1498cf13c09e
ncavchl <ncavchl@ajou.ackr>
2020-04-22 11:26:01 +0900

    header 1차 완성

--- js/common.js
+++ js/common.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 //즉시 실행 함수 예제
+    $('.navi-showbtn').on('click',function(){
+        $('.navi-showbtn img').toggleClass('fold');
+        $("#header").toggleClass('fold');
+        $("#content").toggleClass('fold');
+    });
+
+    $('.subbox-title').on('click',function(){
+        $(this).parents().siblings('.subbox-list').toggleClass('on');
+        $(this).siblings('img').toggleClass('on');
+    });
 
 });
